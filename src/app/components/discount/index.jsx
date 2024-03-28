67c49f5855c0193916e5d73274c8ce4f40eff7a7
nsquansah <ninaspioquansah@gmail.com>
2024-03-28 10:36:20 +0000

    discount card

--- src/app/components/discount/index.jsx
+++
@@ -1,4 +0,0 @@
-import styles from './style.module.css';
-import { discountData } from './data.js';
-import { DiscountCard } from './DiscountCard/index.jsx';
-import { DiscountBanner } from './DiscountBanner/index.jsx';
