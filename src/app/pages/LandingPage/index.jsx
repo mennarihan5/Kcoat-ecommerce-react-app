7ee1ea7f31f083a0143687323080d73e34f5d8f0
nsquansah <ninaspioquansah@gmail.com>
2024-03-29 05:51:30 +0000

    dicount card

--- src/app/pages/LandingPage/index.jsx
+++ src/app/pages/LandingPage/index.jsx
@@ -1,10 +1,12 @@
 import styles from './style.module.css';
 import { Header } from '../../components/Header/index.jsx';
+import Banner from '../../components/discount/index.jsx';
 
 export const LandingPage = () => {
     return (
         <section className={styles.header}>
             <Header />
+            <Banner />
         </section>
     )
 }
