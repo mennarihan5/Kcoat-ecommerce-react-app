6f756d6a5c850691302e52b7a5ace91a5931a136
nsquansah <ninaspioquansah@gmail.com>
2024-03-29 23:46:49 +0000

    new arrival section

--- src/app/components/newarrival/index.jsx
+++ src/app/components/newarrival/index.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
 import styles from './styles.module.css';
+
+
+export const NewItems = () => {
+    return (<div className={styles.arivalcontainer}>
+        <div className={styles.newarrival}>
+            <h1 className={styles.newarrivalText}>New Arrivals</h1>
+        </div>
+            <p className={styles.paragraph}>Explore our curated selection of best selling outfits, jewelry, bags, slides, heels, and more.</p>
+
+        </div>
+    
+    )
+}
+export default NewItems;
--- src/app/pages/LandingPage/index.jsx
+++ src/app/pages/LandingPage/index.jsx
@@ -1,12 +1,14 @@
 import styles from './style.module.css';
 import { Header } from '../../components/Header/index.jsx';
 import Banner from '../../components/discount/index.jsx';
+import {NewItems} from '../../components/newarrival/index.jsx';
 
 export const LandingPage = () => {
     return (
         <section className={styles.header}>
             <Header />
             <Banner />
+            <NewItems />
         </section>
     )
 }
