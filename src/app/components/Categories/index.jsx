1f50d50c661536f3ecf82d29d7a12405c82cf3c8
mennarihan5 <menna1986@live.com>
2024-03-30 13:52:09 +0200

    adding files for trending bestSelling and categories

--- src/app/components/Categories/index.jsx
+++ src/app/components/Categories/index.jsx
@@ -1,4 +1,16 @@
+import { Button } from 'bootstrap';
 import styles from './style.module.css';
+
+export const categoriesCards = () => {
+    return(
+        <div className={styles[`categories-card`]}>
+            <img className={styles.img} />
+            <h3 className={styles.title}></h3>
+            <Button />
+        </div>
+
+    )
+}
 
 export const Categories = () => {
     return(
---
+++ src/app/components/Trending/index.jsx
@@ -0,0 +1,9 @@
+import styles from './style.module.css';
+
+export const Trending = () => {
+    return(
+        <div className={styles.trending}>
+            Trending
+        </div>
+    )
+}
