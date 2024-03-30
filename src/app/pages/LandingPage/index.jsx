ead996e2551d518630babb1632057b66723c83b1
mennarihan5 <menna1986@live.com>
2024-03-30 03:59:40 +0200

    working on categories section

--- src/app/pages/LandingPage/index.jsx
+++ src/app/pages/LandingPage/index.jsx
@@ -3,12 +3,14 @@ import { Header } from '../../components/Header/index.jsx';
 import Banner from '../../components/discount/index.jsx';
 import {NewItems} from '../../components/newarrival/index.jsx';
 import { Hero } from '../../components/Hero/index.jsx';
+import { Categories } from '../../components/Categories/index.jsx';
 
 export const LandingPage = () => {
     return (
         <section className={styles.header}>
             <Header />
             <Hero />
+            <Categories />
             <Banner />
             <NewItems />
         </section>
