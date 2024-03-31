fc85a3aaa39ed413fc0cfe6fe0baf5cc537a76ab
mennarihan5 <menna1986@live.com>
2024-03-31 22:59:53 +0200

    finished catgories section

--- src/app/components/Categories/index.jsx
+++ src/app/components/Categories/index.jsx
@@ -1,21 +1,38 @@
-import { Button } from 'bootstrap';
+import { Button } from '../Button/index';
 import styles from './style.module.css';
+import { cardsData } from './data';
 
-export const categoriesCards = () => {
+export const CategoriesCards = (props) => {
+    const size = props.small ? styles['small-card'] : '';
+    const coverImg = props.cover ? styles['cover-img'] : '';
+    const margin = props.marginTop ? styles['margin-top'] : '';
+    const whiteBtn = props.isWhite ? styles['white-btn'] : '';
+
     return(
-        <div className={styles[`categories-card`]}>
-            <img className={styles.img} />
-            <h3 className={styles.title}></h3>
-            <Button />
+        <div>
+            <div className={`${styles['categories-card']} ${size} ${coverImg} ${margin}`}>
+                <img className={styles.img} src={props.image} alt='card images' />
+                <div className={styles.details}>
+                    <h3 className={styles.title}>{props.title}</h3>
+                    <div className={styles['card-btn']}>
+                        <Button className={`${styles['default-btn']} ${whiteBtn}`} >
+                            Shop Now
+                        </Button>
+                    </div>
+                </div>
+            </div>
         </div>
-
     )
 }
 
-export const Categories = () => {
+export const Categories = (props) => {
     return(
-        <div className={styles.categories}>
-            Categories
-        </div>
+       <div className={styles.categories}>
+            {
+                cardsData.map((data) => {
+                    return <CategoriesCards image={data.image} title={data.title} small={data.small} cover={data.cover} marginTop={data.marginTop} isWhite={data.isWhite}/>
+                })
+            }
+      </div>
     )
 }
--- src/app/pages/LandingPage/index.jsx
+++ src/app/pages/LandingPage/index.jsx
@@ -4,6 +4,8 @@ import Banner from '../../components/discount/index.jsx';
 import {NewItems} from '../../components/newarrival/index.jsx';
 import { Hero } from '../../components/Hero/index.jsx';
 import { Categories } from '../../components/Categories/index.jsx';
+import { Trending } from '../../components/Trending/index.jsx';
+import { cardsData } from '../../components/Categories/data.js';
 
 export const LandingPage = () => {
     return (
@@ -11,6 +13,7 @@ export const LandingPage = () => {
             <Header />
             <Hero />
             <Categories />
+            <Trending />
             <Banner />
             <NewItems />
         </section>
