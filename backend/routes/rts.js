2ea441f7f7d74b48964a6d65264c5a64e72ba5ef
Ceeylla-Favv <bertleypriscy@gmail.com>
2024-04-01 07:54:05 +0100

    connected to database

--- backend/routes/rts.js
+++ backend/routes/rts.js
@@ -1,6 +1,13 @@
 //import router
 const router = require('express').Router();
 
+const {insertCus} = require('../controllers/customerControl')
 const {home} = require('../controllers/users')
+
+
+
 router.get('/', home)
+router.get('/register', insertCus)
+
+
 module.exports = router;
