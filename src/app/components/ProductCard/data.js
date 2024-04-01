dd903dfaa4ff637923babee22e72533b3c38017d
mennarihan5 <menna1986@live.com>
2024-04-01 14:47:28 +0200

    created trending and best selling section

--- src/app/components/ProductCard/data.js
+++ src/app/components/ProductCard/data.js
@@ -6,54 +6,114 @@ import Image5 from '../../assets/images/Rectangle 19.png';
 import Image6 from '../../assets/images/Rectangle 20.png';
 import Image7 from '../../assets/images/Rectangle 21.png';
 import Image8 from '../../assets/images/Rectangle 22.png';
+import Image9 from '../../assets/images/Rectangle 33.png';
+import Image10 from '../../assets/images/Rectangle 34.png';
+import Image11 from '../../assets/images/Rectangle 35.png';
+import Image12 from '../../assets/images/Rectangle 36.png';
+import Image13 from '../../assets/images/Rectangle 19.png';
+import Image14 from '../../assets/images/Rectangle 20.png';
+import Image15 from '../../assets/images/Rectangle 21.png';
+import Image16 from '../../assets/images/Rectangle 22.png';
+
 
 export const productData = [
     {
         id: 1,
         image: Image1,
-        title: "Walk of Freedom",
+        title: "Stylish Outfit",
         price: 5000
     },
     {
         id: 2,
         image: Image2,
-        title: "Walk of Freedom",
+        title: "Elegant Earrings",
         price: 5000
     },
     {
         id: 3,
         image: Image3,
-        title: "Walk of Freedom",
+        title: "Stylish Bag",
         price: 5000
     },
     {
         id:4,
         image: Image4,
-        title: "Walk of Freedom",
+        title: "Classic Slides",
         price: 5000
     },
     {
         id: 5,
         image: Image5,
-        title: "Walk of Freedom",
+        title: "Stylish Outfit",
         price: 5000
     },
     {
         id: 6,
         image: Image6,
-        title: "Walk of Freedom",
+        title: "Elegant Earrings",
         price: 5000
     },
     {
         id: 7,
         image: Image7,
-        title: "Walk of Freedom",
+        title: "Wristwatch",
         price: 5000
     },
     {
         id: 8,
         image: Image8,
-        title: "Walk of Freedom",
+        title: "Classic Heels",
         price: 5000
+    }
+]
+
+ export const bestSellingData =[
+    {
+        id: 1,
+        image: Image9,
+        title: "Clarks Shoes",
+        price: 5500,
+    },
+    {
+        id: 2,
+        image: Image10,
+        title: "Short Dress",
+        price: 5500,
+    },
+    {
+        id: 3,
+        image: Image11,
+        title: "Necklaces",
+        price: 5500,
+    },
+    {
+        id: 4,
+        image: Image12,
+        title: "Luxury bag",
+        price: 5500,
+    },
+    {
+        id: 5,
+        image: Image13,
+        title: "Ladies Pants",
+        price: 5500,
+    },
+    {
+        id: 6,
+        image: Image14,
+        title: "Polka dot Eyeglass",
+        price: 5500,
+    },
+    {
+        id: 7,
+        image: Image15,
+        title: "Beaded Bracelets",
+        price: 5500,
+    },
+    {
+        id: 8,
+        image: Image16,
+        title: "Stiletto Heels",
+        price: 5500,
     }
 ]
