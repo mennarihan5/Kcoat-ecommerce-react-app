88423eb0e54da5ceef1691765683647465daa455
nsquansah <ninaspioquansah@gmail.com>
2024-04-05 14:19:17 +0000

    login page almost done

--- src/app/pages/LandingPage/index.jsx
+++ src/app/pages/LandingPage/index.jsx
@@ -14,6 +14,7 @@ import App from '../../components/customerDetails/index.jsx';
 import { WhatPeopleSay } from '../../components/reviewSection/whatPeopleSay.jsx';
 import AppCards from '../../components/reviewCards/index.jsx';
 import SubscriptionCard from '../../components/newsLetterSection/index.jsx';
+import LoginPage from '../LoginPage/index.jsx';
 
 export const LandingPage = () => {
     return (
@@ -29,6 +30,7 @@ export const LandingPage = () => {
             <WhatPeopleSay />
             <AppCards />
             <SubscriptionCard />
+            <LoginPage />
             {/* <Title /> */}
             
 
