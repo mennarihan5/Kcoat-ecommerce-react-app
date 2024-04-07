fdb4ccf3d2c53fb8f2b63e448d8f15867bd6a1ff
mennarihan5 <menna1986@live.com>
2024-04-07 02:57:15 +0200

    finished about section & working on faq section

--- src/app/components/About/data.js
+++ src/app/components/About/data.js
@@ -1,13 +1,15 @@
-import MenImg from '../../assets/images/man.svg';
+import  MenImg from '../../assets/images/man.svg';
 import WomenImg from '../../assets/images/woman.svg';
 
 export const aboutData = [
     {
-        title: "MEN'S COLLECTION",
+        title1: "MEN'S",
+        title2: "COLLECTION",
         img: MenImg
     },
     {
-        title: "WOMEN'S COLLECTION",
+        title1: "WOMEN'S",
+        title2: "COLLECTION",
         img: WomenImg
     }
 ]
