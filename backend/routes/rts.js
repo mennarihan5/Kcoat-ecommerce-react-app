f719818fa40a65132189af2776d49bb5a0e03922
Ceeylla-Favv <bertleypriscy@gmail.com>
2024-04-11 13:03:23 +0100

    authorization

--- backend/routes/rts.js
+++ backend/routes/rts.js
@@ -3,13 +3,15 @@ const router = require('express').Router();
 
 const {mid} = require('../middleware/mdw')
 const {insertCus} = require('../controllers/customerControl')
-const {home, about, login} = require('../controllers/users')
+const {verifyAuth} = require ('../middleware/auth')
+const {home, about, login, details} = require('../controllers/users')
 
 
 
 router.get('/', mid, home)
 router.get('/about', login)
-router.get('/register', insertCus)
+router.get('/details', verifyAuth, details)
+router.get('/register', insertCus);
 
 
 module.exports = {router};
