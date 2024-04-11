b95a750589655d8ac82097986bee7ef9b6726d1f
nsquansah <ninaspioquansah@gmail.com>
2024-04-11 15:47:04 +0000

    adjustments on new arrival section

--- src/app/App.js
+++ src/app/App.js
@@ -3,6 +3,7 @@ import { AboutPage } from './pages/AboutPage/index.jsx';
 import { LandingPage } from './pages/LandingPage/index.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import { CategoriesPage } from './pages/CategoriesPage/index.jsx';
+import { LoginPage } from './pages/LoginPage/index.jsx';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path='/' element={<LandingPage />}/>
           <Route path='/about' element={<AboutPage />}/>
           <Route path='/categories' element={<CategoriesPage />}/>
+          <Route path='/contact' element={<LoginPage />}/>
         </Routes>
       </BrowserRouter>
     </div>
