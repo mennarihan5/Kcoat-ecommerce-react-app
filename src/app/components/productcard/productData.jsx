7305c8ac3cff01d6d1a4b6006f560381adca82a9
nsquansah <ninaspioquansah@gmail.com>
2024-04-11 17:39:43 +0000

    login and signup styling

--- src/app/components/productcard/productData.jsx
+++ src/app/components/productcard/productData.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { FaHeart } from 'react-icons/fa';
 import styles from './styles.module.css';
+import vector from '../../assets/images/vector.svg';
 
 function ProductCard( {imageSrc, price, description, cardClass, id }) {
   return (
@@ -9,7 +10,9 @@ function ProductCard( {imageSrc, price, description, cardClass, id }) {
       <div className={cardClass} id={id} >
         <div className={styles['product-image']}>
           <img src={imageSrc} alt="Produc]]]t" />
-          <span className={styles['love-icon']}><FaHeart /></span>
+          {/* <span className={styles['love-icon']}><FaHeart /></span> */}
+          <img src={vector} alt='Vector' className={styles.vector} />
+          {/* { <img src=     alt='Vector' className={styles.vector} /> */}
         </div>
         <div className={styles['product-info']}>
         <div className={styles.description}>{description}</div>
