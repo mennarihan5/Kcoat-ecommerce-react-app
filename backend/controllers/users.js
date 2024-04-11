2d3fce1a9156febacff7ea8dd5ed85dbcaf7e339
Ceeylla-Favv <bertleypriscy@gmail.com>
2024-04-11 11:53:53 +0100

    token creation

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const secret = "kcoat"
+const users = {
+    userid: 1,
+    userName: ""
+}
 const home = async(req, res)=>{
     res.status(200).json({message: "message"})
 }
 
-module.exports = {home}
+const about = async(req, res)=>{
+    res.status(200).json({message: "about page"})
+}
+
+const login = async(req,res)=>{
+    const token = jwt.sign(users,secret)
+    console.log(token)
+}
+
+module.exports = {home, about, login}
