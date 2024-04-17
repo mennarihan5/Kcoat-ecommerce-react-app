ee2d3b6e1c687a30e7069988a89b16f8a47105a4
nsquansah <ninaspioquansah@gmail.com>
2024-04-17 20:01:12 +0000

    working on contact page left with overlaping image

--- src/app/App.js
+++ src/app/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CategoriesPage } from './pages/CategoriesPage/index.jsx';
 import { LoginPage } from './pages/LoginPage/index.jsx';
 import { SignupPage } from './pages/SignupPage/index.jsx';
+import { ContactPage } from './pages/ContactPage/index.jsx';
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
           <Route path='/categories' element={<CategoriesPage />}/>
           <Route path='/login' element={<LoginPage />}/> 
           <Route path='/signup' element={<SignupPage />}/>
-          {/* <Route path='/contact' element={<SignupPage />}/> */}
+          <Route path='/contact' element={<ContactPage />}/> 
         </Routes>
       </BrowserRouter>
     </div>
