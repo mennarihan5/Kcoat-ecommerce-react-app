9d483a70f7b809b739f83e62abc7e2f65808905a
Ceeylla-Favv <bertleypriscy@gmail.com>
2024-04-19 12:14:05 +0100

    testing local database

--- backend/routes/auth/index.js
+++ backend/routes/auth/index.js
@@ -3,7 +3,7 @@ const { login, signup, me } = require("../../controllers/auth");
 const { JWTAuthenticationMiddleware } = require("../../middleware/auth");
 
 router.post("/login", login);
-router.post("/signup", signup);
+router.post('/signup', signup);
 router.get("/me", [JWTAuthenticationMiddleware], me);
 
 module.exports = router;
