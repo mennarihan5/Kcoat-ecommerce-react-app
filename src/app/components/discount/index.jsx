7924deafa084dd9a826714fc3914682ad16925c9
mennarihan5 <menna1986@live.com>
2024-04-20 22:00:37 +0200

    edited discount section

--- src/app/components/discount/index.jsx
+++ src/app/components/discount/index.jsx
@@ -2,20 +2,26 @@ import React from 'react';
 import styles from './styles.module.css';
 import discount1 from '../../assets/images/discount1.svg';
 import discount2 from '../../assets/images/discount2.svg';
+import { Link } from 'react-router-dom';
 
 function Banner() {
   return (
     <div className={styles.banner}>
-      <img src={discount1} alt="discount1" className={styles.discount1} />
+      <div className={styles.discountImg}>
+        <img src={discount1} alt="discount1" className={styles.discount1} />
+      </div>
       <section className={styles.discountSection}>
-        <h1 className={styles.discountText}>30% Sales Off
-          <br />  <p className={styles.para}>For every new subscriber</p>
-          <a href="#" className={styles.link}>Shop Now</a>
-        </h1>
+        <div className={styles.discountText}>
+            <h1 className={styles.discountTitle}>30% Sales Off</h1><br />
+            <p className={styles.para}>For every new subscriber</p>
+            <Link href="/categories" className={styles.link}>Shop Now</Link>
+        </div>
         {/* <p className={styles.paragraph}>For every new subscriber</p> */}
         {/* <a href="#" className={styles.link}>Shop Now</a> */}
       </section>
-      <img src={discount2} alt="discount2" className={styles.discount2} />
+      <div className={styles.discountImg}>
+        <img src={discount2} alt="discount2"  />
+      </div>
     </div>
   );
 }
