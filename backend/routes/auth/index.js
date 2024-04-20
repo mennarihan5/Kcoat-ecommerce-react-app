4b5c3dd7b90bb339f25588cfe609848e3ccc2bc8
Ceeylla-Favv <bertleypriscy@gmail.com>
2024-04-20 15:19:41 +0100

    Working on forget password

--- backend/routes/auth/index.js
+++ backend/routes/auth/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { login, signup, me } = require("../../controllers/auth");
 const { JWTAuthenticationMiddleware } = require("../../middleware/auth");
 
-router.post('/login', login);
+router.get('/login', login);
 router.post('/signup', signup);
 router.get('/me', [JWTAuthenticationMiddleware], me);
 
