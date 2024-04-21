dbd7f450b236b506a2932897957bd502e3f9e58b
mennarihan5 <menna1986@live.com>
2024-04-22 00:08:13 +0200

    newArrivals is done

--- src/app/components/productcard/productData.jsx
+++ src/app/components/productcard/productData.jsx
@@ -6,20 +6,18 @@ import vector from '../../assets/images/vector.svg';
 
 function ProductCard( {imageSrc, price, description, cardClass, id }) {
   return (
-    <div className={styles['product-card']}>
-      <div className={cardClass} id={id} >
-        <div className={styles['product-image']}>
-          <img src={imageSrc} alt="Produc]]]t" />
-          {/* <span className={styles['love-icon']}><FaHeart /></span> */}
-          <img src={vector} alt='Vector' className={styles.vector} />
-          {/* { <img src=     alt='Vector' className={styles.vector} /> */}
-        </div>
-        <div className={styles['product-info']}>
-        <div className={styles.description}>{description}</div>
-          <div className={styles.price}>{price}</div>
-         
-        </div>
-      </div>
+    <div className={`${cardClass} ${styles['product-card']}`}>
+
+          <div className={styles['product-image']}>
+              <img src={imageSrc} alt="Product" />
+              {/* <span className={styles['love-icon']}><FaHeart /></span> */}
+              <img src={vector} alt='Vector' className={styles.vector} />
+              {/* { <img src=     alt='Vector' className={styles.vector} /> */}
+          </div>
+          <div className={styles['product-info']}>
+              <div className={styles.description}>{description}</div>
+              <div className={styles.price}>{price}</div>
+          </div>
     </div>
   );
 
