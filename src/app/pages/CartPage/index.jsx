ef517cbcd33f2ce5df95c51b911857c3fdfb219b
nsquansah <ninaspioquansah@gmail.com>
2024-04-22 14:09:02 +0000

    working on cart page

--- src/app/pages/CartPage/index.jsx
+++ src/app/pages/CartPage/index.jsx
@@ -1,15 +1,31 @@
-import react from "react";
-import  styles from './style.module.css';
+import React from "react";
+import styles from './style.module.css';
 import { Header } from '../../components/Header/index.jsx';
 
 export const CartPage = () => {
     return (
         <div className={styles.cart}>
-            <div>
-                <Header />
-            </div>
-            <div>
-                <h1 className={styles.cart}>Cart</h1>
+            <Header />
+            <div className={styles.container}>
+                <div className={styles.leftSection}>
+                    <h2 className={styles.yourCart}>Your Cart</h2>
+                    <p className={styles.items}>3 items</p>
+                </div>
+                <div className={styles.middleSection}>
+                    <div className={styles.item}>
+                        <h2 className={styles.heading1}>Item</h2>
+                    </div>
+                    <div className={styles.quantity}>
+                        <h2 className={styles.heading2}>Quantity</h2>
+                    </div>
+                    <div className={styles.price}>
+                        <h2 className={styles.heading3}>Price</h2>
+                    </div>
+                </div>
+                <div className={styles.rightSection}>
+                    <h2 className={styles.sectionHeading}>Summary</h2>
+                    {/* Add summary and total cost details here */}
+                </div>
             </div>
         </div>
     )
