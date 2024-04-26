49ed82aad7ed11c97afe71e3dd3eaa77494c7349
mennarihan5 <menna1986@live.com>
2024-04-27 01:40:04 +0300

    edits

--- src/app/components/productcard/productData.jsx
+++ src/app/components/productcard/productData.jsx
@@ -1,25 +1,20 @@
 // ProductCard.js
 import React from 'react';
-import { FaHeart } from 'react-icons/fa';
+import { FaRegHeart } from "react-icons/fa6";
 import styles from './styles.module.css';
-import vector from '../../assets/images/vector.svg';
+// import vector from '../../assets/images/vector.svg';
 
 function ProductCard( {imageSrc, price, description, cardClass, id }) {
   return (
-    <div className={styles['product-card']}>
-      <div className={cardClass} id={id} >
-        <div className={styles['product-image']}>
-          <img src={imageSrc} alt="Produc]]]t" />
-          {/* <span className={styles['love-icon']}><FaHeart /></span> */}
-          <img src={vector} alt='Vector' className={styles.vector} />
-          {/* { <img src=     alt='Vector' className={styles.vector} /> */}
-        </div>
-        <div className={styles['product-info']}>
-        <div className={styles.description}>{description}</div>
-          <div className={styles.price}>{price}</div>
-         
-        </div>
-      </div>
+    <div className={`${cardClass} ${styles['product-card']}`}>
+          <div className={styles['product-image']}>
+              <div className={styles.heart}><FaRegHeart /></div>
+              <img src={imageSrc} alt="Product" />
+          </div>
+          <div className={styles['product-info']}>
+              <div className={styles.description}>{description}</div>
+              <div className={styles.price}>{price}</div>
+          </div>
     </div>
   );
 
