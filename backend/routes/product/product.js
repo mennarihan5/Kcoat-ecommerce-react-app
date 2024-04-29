e19d3f93b1b2107c40d5abdd06269bf54a80eeb2
Ceeylla-Favv <bertleypriscy@gmail.com>
2024-04-29 05:16:36 +0100

    worked on uploading images to cloudinary

--- backend/routes/product/product.js
+++ backend/routes/product/product.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const productController = require("../../controllers/auth/productController")
 
 
-router.post("/addProduct", productController.upload.single('image'),productController.addProduct);
+router.post("/addProduct", productController.upload, productController.addProduct);
 router.get("/", productController.getAllProducts);
 router.get("/:id", productController.getOneProduct);
 
