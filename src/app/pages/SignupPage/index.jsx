3e83756a94825139d7ad5539567b36bab79657ad
nsquansah <ninaspioquansah@gmail.com>
2024-04-30 20:13:18 +0000

    signup name bar fixed

--- src/app/pages/SignupPage/index.jsx
+++ src/app/pages/SignupPage/index.jsx
@@ -6,7 +6,7 @@ import { Logo } from '../../components/Logo/index.jsx';
 
 export const SignupPage = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const [successMessage, setSuccessMessage] = useState(null);
+  const [data, setSuccessMessage] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
@@ -26,15 +26,21 @@ export const SignupPage = () => {
         },
         body: JSON.stringify(payload)
       });
+      // const responseData = await response.json();
+      // setData(responseData);
       if (response.ok) {
-        setSuccessMessage("Signup successful!"); // Display success message
+        setSuccessMessage("Signup successful! Taking you to login Page"); // Display success message
+        // redirect to login page
+        setTimeout(() => {
+          window.location.href = '/login';
+        }, 2000);
       } else {
         const responseData = await response.json();
         setError(responseData.message || 'Unknown error occurred');
       }
       setLoading(false);
     } catch (error) {
-      setError(error.message || 'Unknown error occurred');
+      setError(error.message);
       setLoading(false);
     }
   };
@@ -47,58 +53,58 @@ export const SignupPage = () => {
         </div> 
 
         <h2 className={styles.create}>Create your free account</h2>
-        {successMessage ? ( // Display success message if available
-          <p className={styles.successMessage}>{successMessage}</p>
-        ) : (
-          <form className={styles.loginForm} onSubmit={handleSubmit}>
-            <div className={styles.formGroup}>
-              <label className={styles.name}>Name</label>
-              <input type="text" name="full_name" placeholder="Full Name" />
-            </div>
-            <div className={styles.formGroup}>
-              <label className={styles.email}>Email</label>
-              <input type="email" name="email" placeholder="Tech4Dev@kcoat.com" />
-            </div>
-            <div className={styles.passwordformGroup}>
-              <label className={styles.password}>Password</label>
-              <input 
-                type={showPassword ? 'text' : 'password'} 
-                name="password" 
-                placeholder="Enter your password" 
-              />
-              <img 
-                src={eye} 
-                alt="Eye Icon" 
-                className={styles.passwordToggle} 
-                onClick={() => setShowPassword(!showPassword)} 
-              />
-            </div>
+        <form className={styles.loginForm} onSubmit={handleSubmit}>
 
-            <div className={styles.confirmformGroup}>
-              <label className={styles.confirm}>Confirm Password</label>
-              <input 
-                type={showPassword ? 'text' : 'password'} 
-                name="confirmPassword" 
-                placeholder="Confirm your password" 
-              />
-              <img 
-                src={eye} 
-                alt="Eye Icon" 
-                className={styles.passwordToggle} 
-                onClick={() => setShowPassword(!showPassword)} 
-              />          
-            </div>
-            
-            <div className={styles.rememberForgot}>
-              <label>
-                <input type="checkbox" /> Remember me
-              </label>
-              <a href="#" className={styles.forgot}>Forgot Password?</a>
-            </div>
-            <button type="submit">{loading ? 'Signing Up...' : 'Sign Up'}</button>
-          </form>
-        )}
-        {error && <p className={styles.errorMessage}>{error}</p>} {/* Display error message if available */}
+        <div className={styles.formGroup}>
+            <label className={styles.name}>Name</label>
+            <input type="text" name="full_name" placeholder="Full Name" />
+          </div>
+
+          <div className={styles.formGroup}>
+            <label className={styles.email}>Email</label>
+            <input type="email" name="email" placeholder="Tech4Dev@kcoat.com" />
+          </div>
+
+          <div className={styles.passwordformGroup}>
+            <label className={styles.password}>Password</label>
+            <input 
+              type={showPassword ? 'text' : 'password'} 
+              name="password" 
+              placeholder="Enter your password" 
+            />
+            <img 
+              src={eye} 
+              alt="Eye Icon" 
+              className={styles.passwordToggle} 
+              onClick={() => setShowPassword(!showPassword)} 
+            />
+          </div>
+
+          <div className={styles.confirmformGroup}>
+            <label className={styles.confirm}>Confirm Password</label>
+            <input 
+              type={showPassword ? 'text' : 'password'} 
+              name="confirmPassword" 
+              placeholder="Confirm your password" 
+            />
+            <img 
+              src={eye} 
+              alt="Eye Icon" 
+              className={styles.passwordToggle} 
+              onClick={() => setShowPassword(!showPassword)} 
+            />          
+          </div>
+          
+          <div className={styles.rememberForgot}>
+            <label>
+              <input type="checkbox" /> Remember me
+            </label>
+            <a href="#" className={styles.forgot}>Forgot Password?</a>
+          </div>
+          <button type="submit">{loading ? 'Signing Up...' : 'Sign Up'}</button>
+        </form>
+        {error && <p>{error}</p>}
+        {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
         <LoginFormFooter />
       </div>
       <div className={styles.rightSection}></div>
