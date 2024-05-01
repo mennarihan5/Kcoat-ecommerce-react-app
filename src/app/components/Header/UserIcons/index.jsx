06415f1b8bd217a987fb8da55fec9daae68a4c7b
nsquansah <ninaspioquansah@gmail.com>
2024-05-01 22:45:30 +0000

    Aria added to user icons

--- src/app/components/Header/UserIcons/index.jsx
+++ src/app/components/Header/UserIcons/index.jsx
@@ -1,26 +1,25 @@
 import styles from './style.module.css';
-import {ReactComponent as SearchIcon} from '../../../assets/images/search.svg';
-import {ReactComponent as UserIcon} from '../../../assets//images/user.svg';
-import {ReactComponent as HeartIcon} from '../../../assets//images/heart.svg';
-import {ReactComponent as CartIcon} from '../../../assets//images/cart.svg';
+import { ReactComponent as SearchIcon } from '../../../assets/images/search.svg';
+import { ReactComponent as UserIcon } from '../../../assets/images/user.svg';
+import { ReactComponent as HeartIcon } from '../../../assets/images/heart.svg';
+import { ReactComponent as CartIcon } from '../../../assets/images/cart.svg';
 import { Link } from 'react-router-dom';
 
 export const UserIcons = (props) => {
-    // const login = props.login ? 
-    return(
-        <div className={styles[`icons-wrapper`]}>
-                <div className={styles.icon}>
-                    <SearchIcon />
-                </div>
-                <div className={styles.icon}>
-                    <Link to="/login"><UserIcon /></Link>
-                </div>
-                <div className={styles.icon}>
-                    <Link to="/favorites"><HeartIcon /></Link>
-                </div>
-                <div className={styles.icon}>
-                    <Link to="/cart"><CartIcon /></Link>
-                </div>
+    return (
+        <div className={styles[`icons-wrapper`]} role="navigation" aria-label="User Navigation">
+            <div className={styles.icon} aria-label="Search">
+                <SearchIcon />
+            </div>
+            <div className={styles.icon} aria-label="User">
+                <Link to="/login"><UserIcon /></Link>
+            </div>
+            <div className={styles.icon} aria-label="Favorites">
+                <Link to="/favorites"><HeartIcon /></Link>
+            </div>
+            <div className={styles.icon} aria-label="Cart">
+                <Link to="/cart"><CartIcon /></Link>
+            </div>
         </div>
     )
 }
